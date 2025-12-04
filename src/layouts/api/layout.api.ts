e3f2ccb9a940a4f5f76317bf146721a373052bd5
Manolo Dorado <manuel.dorado@lemoncode.net>
2025-12-04 17:15:37 +0100

    feat: add language support; implement Language model and update components for dynamic language handling

--- src/layouts/api/layout.api.ts
+++ src/layouts/api/layout.api.ts
@@ -1,14 +1,17 @@
+import type { Language } from '#common/models';
 import client from '#lib/client.ts';
 import type { Footer, Header } from './layout.api-model';
 
-export const getHeader = async (): Promise<Header> =>
+export const getHeader = async (lang: Language): Promise<Header> =>
   await client.getContent<Header>({
     contentType: 'Header',
+    language: lang,
     includeRelatedContent: true,
   });
 
-export const getFooter = async (): Promise<Footer> =>
+export const getFooter = async (lang: Language): Promise<Footer> =>
   await client.getContent<Footer>({
     contentType: 'Footer',
+    language: lang,
     includeRelatedContent: true,
   });
--- src/pods/home/api/home.api.ts
+++ src/pods/home/api/home.api.ts
@@ -1,26 +1,30 @@
-import type { Meta } from '#common/models';
+import type { Language, Meta } from '#common/models';
 import client from '#lib/client.ts';
 import type { ContactSection, HeroSection, ServicesSection } from './home.api-model';
 
-export const getContactSection = async (): Promise<ContactSection> =>
-  await client.getContent<ContactSection>({
-    contentType: 'ContactSection',
-    includeRelatedContent: true,
-  });
-
-export const getHeroSection = async (): Promise<HeroSection> =>
+export const getHeroSection = async (lang: Language): Promise<HeroSection> =>
   await client.getContent<HeroSection>({
     contentType: 'HeroSection',
+    language: lang,
     includeRelatedContent: true,
   });
 
-export const getServicesSection = async (): Promise<ServicesSection> =>
+export const getServicesSection = async (lang: Language): Promise<ServicesSection> =>
   await client.getContent<ServicesSection>({
     contentType: 'ServicesSection',
+    language: lang,
     includeRelatedContent: true,
   });
 
-export const getHomeMeta = async (): Promise<Meta> =>
+export const getContactSection = async (lang: Language): Promise<ContactSection> =>
+  await client.getContent<ContactSection>({
+    contentType: 'ContactSection',
+    language: lang,
+    includeRelatedContent: true,
+  });
+
+export const getHomeMeta = async (lang: Language): Promise<Meta> =>
   await client.getContent<Meta>({
     id: '6931a3f6a89fe07e8789dc3f',
+    language: lang,
   });
