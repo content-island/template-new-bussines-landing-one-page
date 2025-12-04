0f52108375523ef3543729f21855446a39d0a35c
Manolo Dorado <manuel.dorado@lemoncode.net>
2025-12-04 11:03:09 +0100

    fix: update Header model to replace title with brand and adjust Header component accordingly

--- src/layouts/api/layout.api-model.ts
+++ src/layouts/api/layout.api-model.ts
@@ -5,7 +5,7 @@ export interface Header {
   id: string;
   language: 'en';
   lastUpdate: string; // Stores the date in ISO 8601 format. For example: 2021-09-10T19:30:00.000Z
-  title: string;
+  brand: NavigationItem;
   home: NavigationItem;
   services: NavigationItem;
   contact: NavigationItem;
